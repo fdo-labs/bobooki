44118d5099699de1b5aa027e63b170b97098cf94
hesslau <henrik@hesslau.org>
2021-11-09 13:33:38 +0100

    MassUploadImportWorker was missing files created after 7am. this commit improves the algorithm to prevent this

--- app/workers/mass_uploads_import_worker.rb
+++ app/workers/mass_uploads_import_worker.rb
@@ -13,7 +13,7 @@ class MassUploadsImportWorker
   # `uri_pattern` should have the format "http://10.0.0.4/export/Export-{DATE}-{COUNT}.csv"
   # whereas {DATE} will be replaced with todays date pattern (%Y%m%d)
   # and {COUNT} will be replaced with an incremented integer (starting from 0)
-  def perform (uri_pattern, user_id, start_counting_from = 0)
+  def perform (uri_pattern, user_id)
 
     user_id = user_id.to_i
     uri_pattern = uri_pattern.to_s
@@ -25,18 +25,36 @@ class MassUploadsImportWorker
       return response.code.to_i == 200
     end
 
-    date = Time.new.strftime("%Y%m%d")
-    todays_uri_pattern = uri_pattern.gsub("{DATE}",date)
+    def get_files(pattern)
+      files = []
+      count = 0
 
+      loop do
+        uri = pattern.gsub("{COUNT}",count.to_s)
+        break unless remote_file_exists?(uri)
+        files << uri
+        count+=1
+      end
+
+      files
+    end
+
+    # get list of today's files
+    # and to make sure we didn't miss anything, also yesterday's files
+    todays_uri_pattern = uri_pattern.gsub("{DATE}",0.day.ago.strftime("%Y%m%d"))
+    yesterdays_uri_pattern = uri_pattern.gsub("{DATE}",1.day.ago.strftime("%Y%m%d"))
+    remote_files = get_files(yesterdays_uri_pattern) + get_files(todays_uri_pattern)
+
+    # remove files which were already imported
+    remote_files.reject! do |rf|
+      MassUpload.find_by(file_file_name: rf.split("/").last.downcase)
+    end
+
+    # import remaining files
     user = User.find(user_id)
-    count = start_counting_from
-    uri = todays_uri_pattern.gsub("{COUNT}",count.to_s)
-
-    while remote_file_exists?(uri) do
-      mass_upload = user.mass_uploads.build({file: uri})
+    remote_files.each do |rf|
+      mass_upload = user.mass_uploads.build({file: rf})
       mass_upload.process if mass_upload.save
-      count+=1
-      uri = todays_uri_pattern.gsub("{COUNT}",count.to_s)
     end
   end
 end
